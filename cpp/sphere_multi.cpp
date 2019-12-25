561934456a9b67f4a4c2100dec2511951a8c0c1a
cygnusbeta <betacygnib+git@gmail.com>
2019-12-25 09:23:27 +0900

    Use log scale plot on sphere_multi.cpp

--- cpp/sphere_multi.cpp
+++ cpp/sphere_multi.cpp
@@ -5,27 +5,27 @@
 using namespace std;
 
 inline int main_inline() {
-    const int n = int(pow(10, 8)), num_plot = 100, interval = n / num_plot;
+    const int n_pow = 8, n = int(pow(10, n_pow)), num_plot = 100, interval = n / num_plot;
     const int d_max = 20;
+
     for (int d = 2; d <= d_max; d++) {
-        int count = 0;
+        int i_log = 0, count = 0;
         double r2s[n];
         string s;
         for (int i = 0; i < n; i++) {
             Sample sample(d);
             if (sample.run()) count++;
 
-            if (i % interval == interval - 1 or i == n - 1) {
+            double log10_i = log10(i + 1);
+            if (log10_i >= double(n_pow) / double(num_plot) * double(i_log) or i == n - 1) {
                 r2s[i] = sample.r2;
                 double v = double(count) / double(i + 1) * pow(2, d);
                 double p = double(count) / double(i + 1);
                 double q = 1.0 - p;
                 double error = 1.96 * pow(2, d) * sqrt(p * q / double(i + 1));
                 printf("d = %d, i + 1 = %d, v = %f ± %f\n", d, i + 1, v, error);
-                if (d == 4) {
-                    true;
-                }
-                s += to_string(i + 1) + "," + to_string(v) + "," + to_string(error) + "\n";
+                s += to_string(log10_i) + "," + to_string(v) + "," + to_string(error) + "\n";
+                i_log++;
             }
         }
         string fpath = "../out/r2_plot_" + to_string(d) + "d.csv";
