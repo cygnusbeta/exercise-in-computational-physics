ff43021bdb0532ece262f14356d9e18a69b4f6e1
cygnusbeta <betacygnib+git@gmail.com>
2020-01-10 15:27:43 +0900

    Add fall1,2,3.cpp

---
+++ cpp/numeric_calculation/fall1.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+#include "../util.h"
+
+using namespace std;
+
+int main() {
+    const float g = -9.8;
+    double v = 20.0, x = 0.0, t = 0.0, dt = 0.1;
+    string s;
+    s += to_string(t) + " " + to_string(x) + "\n";
+    for (int n = 0; n < 40; n++) {
+        t = dt * double(n + 1);
+        x += v * dt;
+        v += g * dt;
+        s += to_string(t) + " " + to_string(x) + "\n";
+    }
+    string fpath = "../out/fall.dat";
+    write_to_file(s, fpath);
+
+    return 0;
+}
---
+++ cpp/numeric_calculation/fall2.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <string>
+#include "../util.h"
+
+using namespace std;
+
+int main() {
+    const float g = -9.8, theta = (1.0 / 4.0) * M_PI, v = 20.0;
+    double t = 0.0, dt = 0.1, x = 0.0, v_x = v * cos(theta), z = 0.0, v_z = v * sin(theta);
+    string s;
+    s += to_string(x) + " " + to_string(z) + "\n";
+    double tmax = 4.0;
+    for (int n = 0; n < tmax / dt; n++) {
+        t = dt * double(n + 1);
+        z += v_z * dt;
+        v_z += g * dt;
+        x += v_x * dt;
+        v_z += 0 * dt;
+        s += to_string(x) + " " + to_string(z) + "\n";
+    }
+    string fpath = "../out/fall2.dat";
+    write_to_file(s, fpath);
+
+    return 0;
+}
