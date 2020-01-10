ec3bc9844809e8ae4102884102778ada414845fa
cygnusbeta <betacygnib+git@gmail.com>
2020-01-10 15:28:31 +0900

    Add oscil4

---
+++ cpp/numeric_calculation/oscil4.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+#include "../util.h"
+
+using namespace std;
+
+int main() {
+    double v = 0.0, x = 1.0, t = 0.0, dt = 0.1;
+    string s;
+    s += to_string(t) + " " + to_string(x) + "\n";
+    for (int n = 0; n < 100; n++) {
+        t = dt * double(n + 1);
+        double x_tmp = x;
+        x += v * dt;
+        v += -x_tmp * dt;
+        s += to_string(t) + " " + to_string(x) + "\n";
+    }
+    string fpath = "../out/oscil4.dat";
+    write_to_file(s, fpath);
+
+    return 0;
+}
