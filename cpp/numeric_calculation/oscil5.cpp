28a763572cc40f2a7f4cff08372099ef60526b3c
cygnusbeta <betacygnib+git@gmail.com>
2020-01-10 15:52:50 +0900

    Add oscil4-7

---
+++ cpp/numeric_calculation/oscil5.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <string>
+#include "../util.h"
+
+using namespace std;
+
+int main() {
+    double v = 0.0, x = 1.0, t = 0.0, dt = 0.1;
+    string s;
+    s += to_string(t) + " " + to_string(x) + "\n";
+    for (int n = 0; n < 100; n++) {
+        t = dt * double(n + 1);
+        double x_tmp = x;
+        x += v * dt;
+        v += -x_tmp * dt;
+        double diff = (x - cos(t)) / cos(t);
+        s += to_string(t) + " " + to_string(diff) + "\n";
+    }
+    string fpath = "../out/oscil5.dat";
+    write_to_file(s, fpath);
+
+    return 0;
+}
