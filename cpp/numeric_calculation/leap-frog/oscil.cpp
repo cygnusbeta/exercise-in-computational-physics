2e656bf41fc75b06277b481ce253177d17055a7c
cygnusbeta <betacygnib+git@gmail.com>
2020-01-21 08:52:13 +0900

    Fix leap-frog initial condition

--- cpp/numeric_calculation/leap-frog/oscil.cpp
+++ cpp/numeric_calculation/leap-frog/oscil.cpp
@@ -5,14 +5,15 @@
 using namespace std;
 
 int main() {
-    double v = 0.0, x = 1.0, t = 0.0, dt = 0.1;
+    double v = 0.0, x = 1.0, t = 0.0, dt = 0.1, dt2 = dt * 0.5, t2 = dt2;
+    v += -x * dt * 0.5;
     string s;
     s += to_string(t) + " " + to_string(x) + "\n";
     for (int n = 0; n < 100; n++) {
         t = dt * double(n + 1);
         x += v * dt;
         s += to_string(t) + " " + to_string(x) + "\n";
-        t = dt * double(n + 1.0 + 0.5);
+        t2 = dt * double(n + 1.0 + 0.5);
         v += -x * dt;
     }
     string fpath = "../out/leap-frog/oscil.dat";
