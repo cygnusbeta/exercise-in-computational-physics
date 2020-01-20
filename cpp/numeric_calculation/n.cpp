5c8ef1d743d1c480721a42227f24bd4c112c2a64
cygnusbeta <betacygnib+git@gmail.com>
2020-01-21 08:12:05 +0900

    Fix index out of bounds

--- cpp/numeric_calculation/n.cpp
+++ cpp/numeric_calculation/n.cpp
@@ -8,7 +8,7 @@ int main() {
     int n = 2000;
     double x[n], y[n], v_x[n], v_y[n];
     x[0] = 0.5, y[0] = 0.0, v_x[0] = 0.0, v_y[0] = 1.63;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         x[i + 1] = x[i] + v_x[i] * dt;
         y[i + 1] = y[i] + v_y[i] * dt;
         double r = sqrt(pow(x[i], 2) + pow(y[i], 2));
