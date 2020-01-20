675020b146eecdfb9d62d63c01cf084c09afbeb0
cygnusbeta <betacygnib+git@gmail.com>
2020-01-21 07:49:26 +0900

    Add n.dat (euler)

---
+++ cpp/numeric_calculation/n.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include "../util.h"
+
+using namespace std;
+
+int main() {
+    const double dt = 0.1, G = 1.0, M = 1.0, m = 1.0;
+    int n = 2000;
+    double x[n], y[n], v_x[n], v_y[n];
+    x[0] = 0.5, y[0] = 0.0, v_x[0] = 0.0, v_y[0] = 1.63;
+    for (int i = 0; i < n; i++) {
+        x[i + 1] = x[i] + v_x[i] * dt;
+        y[i + 1] = y[i] + v_y[i] * dt;
+        double r = sqrt(pow(x[i], 2) + pow(y[i], 2));
+        v_x[i + 1] = v_x[i] - G * M * m * x[i] / (pow(r, 3) * m) * dt;
+        v_y[i + 1] = v_y[i] - G * M * m * y[i] / (pow(r, 3) * m) * dt;
+    }
+    string s;
+    for (int i = 0; i < n; i++) {
+        s += to_string(x[i]) + " " + to_string(y[i]) + "\n";
+    }
+    string fpath = "../out/n.dat";
+    write_to_file(s, fpath);
+
+//    gnuplot で
+//    ```set size ratio -1
+//       plot 'n.dat'```
+//    でプロット
+    return 0;
+}
