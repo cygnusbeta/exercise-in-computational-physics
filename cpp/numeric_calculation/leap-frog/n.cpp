55ee5bbce054fd770776b2f0bbd2cb6b4ad031bb
cygnusbeta <betacygnib+git@gmail.com>
2020-01-21 11:37:56 +0900

    Improve initial condition definition

--- cpp/numeric_calculation/leap-frog/n.cpp
+++ cpp/numeric_calculation/leap-frog/n.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 int main() {
-    const double dt = 0.1, G = 1.0, M = 1.0, m = 1.0;
-    const int n = 100;
+    const int tmax = 100;
+    const int n = 1000;
+    const double dt = double(tmax) / double(n), G = 1.0, M = 1.0, m = 1.0;
     double x[2 * n], y[2 * n], v_x[2 * n], v_y[2 * n];
     x[0] = 0.5, y[0] = 0.0, v_x[0] = 0.0, v_y[0] = 1.63;
 
     double r = sqrt(pow(x[0], 2) + pow(y[0], 2));
     v_x[1] = v_x[0] - G * M * m * x[0] / (pow(r, 3) * m) * dt * 0.5;
     v_y[1] = v_y[0] - G * M * m * y[0] / (pow(r, 3) * m) * dt * 0.5;
-    for (int i = 0; i < n - 1; i++) {
+    for (long i = 0; i < n - 1; i++) {
         x[2 * (i + 1)] = x[2 * i] + v_x[2 * i + 1] * dt;
         y[2 * (i + 1)] = y[2 * i] + v_y[2 * i + 1] * dt;
         r = sqrt(pow(x[2 * (i + 1)], 2) + pow(y[2 * (i + 1)], 2));
