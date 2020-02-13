fb7d96e3a0ceb2125a9ed7a1d12b71b9f67294b7
cygnusbeta <betacygnib+git@gmail.com>
2020-02-13 21:44:44 +0900

    Implement threading

--- cpp/dice_game/hello_multithread.cpp
+++ cpp/dice_game/hello_multithread.cpp
@@ -1,58 +1,79 @@
 #include <vector>
 #include <atomic>
 #include <thread>
+#include "../util2.h"
+
+using namespace std;
 
 using container = std::vector<std::atomic<double>>;
 using container_size_type = container::size_type;
 
 container c(100);
 
-std::atomic<container::pointer> p_busy_elem{ nullptr };
+std::atomic<container::pointer> p_busy_elem_from{ nullptr };
+std::atomic<container::pointer> p_busy_elem_to{ nullptr };
+
+bool try_change(int giveFrom, int giveTo, bool breaked) {
+    // A1: wait for editor thread to finish editing value
+    while (p_busy_elem_from == &c[giveFrom] || p_busy_elem_to == &c[giveTo])
+    {
+        // A2: room a better algorithm to prevent blocking/yielding
+        std::this_thread::yield();
+    }
+
+    p_busy_elem_from.exchange(&c[giveFrom]); // c[i] is busy
+    p_busy_elem_to.exchange(&c[giveTo]); // c[i] is busy
+    // ... edit c[i] ... // E: calculate a value and assign it to c[i]
+    // B: if c[i] is updated in between A and B, this will load the latest value
+    auto valueFrom = c[giveFrom].load();
+    auto valueTo = c[giveTo].load();
+    // C: c[i] might have changed by this time, but we had the most up to date value we could get without checking again
+    // ... use value ...
+    if (c[giveFrom] != 0) {
+        c[giveFrom] = valueFrom - 1;
+        c[giveTo] =  valueTo + 1;
+        breaked = true;
+    }
+    p_busy_elem_from.exchange(nullptr); // c[i] is no longer busy
+    p_busy_elem_to.exchange(nullptr); // c[i] is no longer busy
+    return breaked;
+}
 
 void editor()
 {
-    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i)
-    {
-        p_busy_elem.exchange(&c[i]); // c[i] is busy
-        // ... edit c[i] ... // E: calculate a value and assign it to c[i]
-        c[i] = c[i] * 2;
-        p_busy_elem.exchange(nullptr); // c[i] is no longer busy
-    }
-}
-
-void reader()
-{
-    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i)
-    {
-        // A1: wait for editor thread to finish editing value
-        while (p_busy_elem == &c[i])
-        {
-            // A2: room a better algorithm to prevent blocking/yielding
-            std::this_thread::yield();
+    int giveFrom, giveTo;
+    bool breaked = false;
+    while (!breaked) {
+        giveFrom = get_random_0_to_n(c.size());
+        giveTo = get_random_0_to_n(c.size());
+        if (giveTo != giveFrom) {
+            breaked = try_change(giveFrom, giveTo, breaked);
         }
-
-        // B: if c[i] is updated in between A and B, this will load the latest value
-        auto value = c[i].load();
-
-        // C: c[i] might have changed by this time, but we had the most up to date value we could get without checking again
-        // ... use value ...
-        printf("%lf\n", value);
     }
 }
 
 int main()
 {
-    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i)
-    {
+    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i) {
         c[i] = i;
     }
-    std::thread t_editor{ editor };
-    std::thread t_reader{ reader };
-    t_editor.join();
-    t_reader.join();
+//    std::thread t_editor{ editor };
+//    std::thread t_reader{ reader };
+//    t_editor.join();
+//    t_reader.join();
+
+    // spawn n threads:
+    int nExchange = 10000;
+    vector<thread> threads(nExchange);
+    for (int iThread = 0; iThread < nExchange; iThread++) {
+        threads[iThread] = std::thread { editor };
+    }
+    for (auto& t : threads) {
+        t.join();
+    }
     int sum = 0;
-    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i)
-    {
+    for (container_size_type i{ 0 }, sz{ c.size() }; i < sz; ++i) {
+        printf("c[%d] = %d\n", i, int(c[i]));
         sum += c[i];
     }
     printf("sum = %d\n", sum);
